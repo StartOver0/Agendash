8413ced518e56e3398dd486d560416d15a3f700a
Harshit Rawat <110087852+StartOver0@users.noreply.github.com>
2025-08-11 14:48:05 +0530

    Delete server/agenda directory

--- server/agenda/jobs/index.js
+++
@@ -1,7 +0,0 @@
-// This file imports and registers all jobs
-module.exports = function (agenda) {
-    require("./emailJob")(agenda);
-    require("./cleanupJob")(agenda);
-    require("./reportJob")(agenda);
-  };
-  
